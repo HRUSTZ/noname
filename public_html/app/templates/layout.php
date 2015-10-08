6d609a0f6bf686495d33c9c2e073c0ced7b8bdc8
German Gorshkov <23lopushok@gmail.com>
2015-10-08 18:14:43 +0300

    layout basics

--- public_html/app/templates/layout.php
+++ public_html/app/templates/layout.php
@@ -1,3 +1,21 @@
-ХЕДЕР
-<?= $content ?>
-ФУТЕР
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="app/templates/style.css">
+  <title></title>
+</head>
+<body>
+  <div class="container">
+    <header>
+      Хэдер  
+    </header>
+
+    <?= $content ?>
+    
+    <footer>
+      Футер
+    </footer>
+  </div>
+</body>
+</html>
