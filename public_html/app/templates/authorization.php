28e98258f168a92d3da1425092589a3256d56fa0
HRUSTZ <hawk_vladimir@inbox.ru>
2015-10-12 22:36:25 +0300

    fix

--- public_html/app/templates/authorization.php
+++ public_html/app/templates/authorization.php
@@ -1,4 +1,4 @@
-﻿<form action="/authorization" method="POST" class="form-auth">
+<form action="/authorization" method="POST" class="form-auth">
   <input type="text" name="email" id="email" placeholder="Email:"/>
   <input type="password" name="password" id="password" placeholder="Пароль:"/>
   <input type="submit" value="Вход"  class="button-auth">
