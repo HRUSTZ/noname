20dbeba0e56e8e58454ea6111e7cf654a331d705
German Gorshkov <23lopushok@gmail.com>
2015-10-18 00:16:30 +0300

    fix

--- public_html/app/templates/layout.php
+++ public_html/app/templates/layout.php
@@ -50,8 +50,8 @@
               <form action="/authorization" method="POST" class="form-auth">
                 <input type="text" name="login" id="login" placeholder="Email:"/>
                 <input type="password" name="password" id="password" placeholder="Пароль:"/>
-                <? $_SESSION['link'] = $_SERVER['REQUEST_URI'] ?>
                 <input type="submit" value="Вход"  class="button-auth">
+                <? $_SESSION['link'] = $_SERVER['REQUEST_URI'] ?>
               </form>
             </section>
             <a class="registr" href="/registration"><span>Регистрация</span></a>
