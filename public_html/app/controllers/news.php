ebe82a9fc27a19ffc5c379ccb8da7b7eb3d17260
HRUSTZ <hawk_vladimir@inbox.ru>
2015-10-19 23:52:38 +0300

    news new version

--- public_html/app/controllers/news.php
+++ public_html/app/controllers/news.php
@@ -1,7 +1,47 @@
 <?
-$id = $query[2];
+$control = $query[2];
+$id = $query[3];
 global $mysqli;
 
+if($control == '') $control = 'page';
+
+if($control == 'article'){
+  if($id){
+    $q = $mysqli -> query("SELECT * FROM News WHERE id='$id'");
+    $result = $q -> fetch_assoc();
+    
+    if($result){   
+      $title = $result[name];
+      $content = tpl("article", $result);
+    } else{
+      error('Нет статьи');
+    }
+  } else{
+    error('Нет статьи');
+  }
+} elseif($control == 'page'){
+  $q = $mysqli -> query("SELECT COUNT(id) FROM News");
+  $count = $q -> fetch_row();
+  
+  if(!$id){
+    $id = '1';
+    $q = $mysqli -> query("SELECT * FROM News ORDER BY id DESC LIMIT 0, 5");
+    $result = $q -> fetch_all(MYSQLI_ASSOC);
+  } else{
+    $start = ($id - 1) * 5;
+    $q = $mysqli -> query("SELECT * FROM News ORDER BY id DESC LIMIT $start, 5");
+    $result = $q -> fetch_all(MYSQLI_ASSOC);
+  }
+  
+  $title = "Новости";
+  $content = tpl("articles", $result);
+} else{
+  error ('404');
+}
+
+
+
+/*
 if($id) {
   $q = $mysqli->query("SELECT * FROM News WHERE id='$id'");
   $result = $q->fetch_assoc();
@@ -18,4 +58,4 @@ if($id) {
   
   $title = "Новости";
   $content = tpl("articles", $result);
-}
+}*/
