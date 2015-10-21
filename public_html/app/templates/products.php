c56c6650e2129f6953f1fa72c54f16a737ac33bf
German Gorshkov <23lopushok@gmail.com>
2015-10-21 09:50:52 +0300

    products

--- public_html/app/templates/products.php
+++ public_html/app/templates/products.php
@@ -1,8 +1,23 @@
-<? foreach($cont as $value): ?>
-  <?= $value[id]?>
-  <?= $value[name]?>
-  <?= $value[description]?>
-  <img src="<?= $value[photo]?>">
-<? endforeach; ?>
+<section class="block-products">
+  <div class="wrap">
+    <section class="products-content flex">
+      
+      <? foreach($cont as $value): ?>
+      <article class="product">
+        <img class="product-img" src="<?= $value[photo]?>" alt="<?= $value[name]?>"></a>
+        <div class="product-info flex">
+          <a class="product-name" href="#"><?= $value[name]?></a>
+          <p class="product-description"><?= $value[description]?></p>
+          <button class="product-btn">Купить</button>
+        </div>
+      </article>
+      <? endforeach; ?>
+    
+    </section>
 
-<?page($next[p1], $next[p2], $next[p3], 12);?>
+    <?page($next[p1], $next[p2], $next[p3], 12);?>
+
+  </div>
+</section>
+        <?= $value[id]?>
+        
