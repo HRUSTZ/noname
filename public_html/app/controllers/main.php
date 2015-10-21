08106c4332f685abda7e1058da283320c52da7bc
HRUSTZ <hawk_vladimir@inbox.ru>
2015-10-21 22:13:22 +0300

    main

--- public_html/app/controllers/main.php
+++ public_html/app/controllers/main.php
@@ -1,4 +1,14 @@
 <?
 global $mysqli;
+
+$q = $mysqli -> query("SELECT maintext FROM Data");
+$result[text] = $q -> fetch_assoc();
+
+$q = $mysqli -> query("SELECT * FROM News ORDER BY id DESC LIMIT 0, 5");
+$result[news] = $q -> fetch_all(MYSQLI_ASSOC);
+
+$q = $mysqli -> query("SELECT id, name, photo, price, main  FROM Products WHERE main = '1' || main = '2' || main = '3' ORDER BY main");
+$result[products] = $q -> fetch_all(MYSQLI_ASSOC);
+
 $title = "Главная страница";
 $content = tpl("main",$result);
--- public_html/app/templates/main.php
+++ public_html/app/templates/main.php
@@ -3,18 +3,17 @@
     
     <section class="block-about">
       <h1>О компании</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-      Nostrum quas repudiandae doloribus reprehenderit quis cupiditate molestias explicabo fugiat corporis. 
-      Error tenetur velit minus laborum alias eum praesentium, aut dolor iure, harum quasi omnis ut saepe 
-      iste labore optio sed, deleniti voluptates! Reprehenderit, tenetur. Praesentium recusandae sint dolore 
-      a distinctio quibusdam. Porro fugit labore minus amet in, culpa optio, rerum. Dicta autem, soluta 
-      dolores nulla, velit quo maxime! In, sit cupiditate vitae, aut quis, ea minus alias quasi ducimus eu
-      sunt? Sint quisquam commodi officiis modi temporibus recusandae pariatur quas, consequatur, voluptatibus 
-      vitae quia iste minima explicabo sunt veritatis dolorum ut?</p>
+      <p><?= $cont[text][maintext]?></p>
     </section>
 
     <section class="block-last-news">
       <h1>Последние новости</h1>
+      <? foreach($cont[news] as $value): ?>
+      <?= $value[id]?>
+      <?= $value[name]?>
+      <?= $value[photo]?>
+      <?= $value[price]?>
+      <? endforeach; ?>
       <div class="last-news flex">
         <article class="last-post">        
 
@@ -31,6 +30,12 @@
 
     <section class="block-pop-products">
       <h1>Популярные товары</h1>
+      <? foreach($cont[products] as $value): ?>
+      <?= $value[id]?>
+      <?= $value[name]?>
+      <?= $value[photo]?>
+      <?= $value[price]?>
+      <? endforeach; ?>
       <div class="pop-products flex">
         <article class="pop-product">        
 
