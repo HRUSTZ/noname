148d80764bf898448520ad16b48f3fac95fba4fc
HRUSTZ <hawk_vladimir@inbox.ru>
2015-10-21 23:39:16 +0300

    panel

--- public_html/app/controllers/main.php
+++ public_html/app/controllers/main.php
@@ -4,7 +4,7 @@ global $mysqli;
 $q = $mysqli -> query("SELECT maintext FROM Data");
 $result[text] = $q -> fetch_assoc();
 
-$q = $mysqli -> query("SELECT * FROM News ORDER BY id DESC LIMIT 0, 5");
+$q = $mysqli -> query("SELECT * FROM News ORDER BY id DESC LIMIT 0, 3");
 $result[news] = $q -> fetch_all(MYSQLI_ASSOC);
 
 $q = $mysqli -> query("SELECT id, name, photo, price, main  FROM Products WHERE main = '1' || main = '2' || main = '3' ORDER BY main");
--- public_html/app/controllers/panel.php
+++ public_html/app/controllers/panel.php
@@ -1,16 +1,19 @@
 <?
 if(!$admin){error("Ты не админ!");}
 
-global $mysqli;
-$control = $query[2];
-
-if($control == 'add_news'){
-  print_r(sdf);
-  $title="Панель управления";
-  $content=tpl("add_news", $result);
+if($query[2]){
+  $controller = ROOT."/app/controllers/panel_".$query[2].".php";
+  if(!include($controller)){
+    error('Нет страницы - '.$controller);
+  }
 } else{
-  error('404');
+  $q = $mysqli -> query("SELECT COUNT(id) FROM News");
+  $count = $q -> fetch_row();
+  $result[0] = $count[0];
+  $q = $mysqli -> query("SELECT COUNT(id) FROM Products");
+  $count = $q -> fetch_row();
+  $result[1] = $count[0];
+  
+  $title="Панель управления";
+  $content=tpl("panel", $result);
 }
-
-$title="Панель управления";
-$content=tpl("panel", $result);
