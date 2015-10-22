a49a5f111614565e3690d1acf1df65f7914ebaa6
German Gorshkov <23lopushok@gmail.com>
2015-10-23 00:10:37 +0300

    products

--- public_html/app/templates/products.php
+++ public_html/app/templates/products.php
@@ -1,24 +1,11 @@
-<section class="block-products">
-  <div class="wrap">
-    <section class="products-content flex">
-      
-      <? foreach($cont as $value): ?>
-      <article class="product">
-        <img class="product-img" src="<?= $value[photo]?>" alt="<?= $value[name]?>"></a>
-        <div class="product-info flex">
-          <a class="product-name" href="/products/number/<?= $value[id]?>"><?= $value[name]?></a>
-          <p class="product-description"><?= $value[description]?></p>
-          <p class="product-price"><?= $value[price]?> руб.</p>
-          <a class="more-product-btn flex" href="/products/number/<?= $value[id]?>">Подробнее</a>
-        </div>
-      </article>
-      <? endforeach; ?>
-    
-    </section>
-
-    <?page($next[p1], $next[p2], $next[p3], 12);?>
-
-  </div>
-</section>
-       
-        
+<section class="block-products wrap flex">
+  <? foreach($cont as $value): ?>         
+  <article class="product flex">
+    <a class="product-img" href="/products/number/<?= $value[id]?>"><img src="<?= $value[photo]?>" alt="<?= $value[name]?>"></a>
+    <a class="product-name" href="/products/number/<?= $value[id]?>"><?= $value[name]?></a>
+    <p class="product-description"><?= $value[description]?></p>
+    <p class="product-price"><?= $value[price]?> руб.</p>
+  </article>
+  <? endforeach; ?>
+  <?page($next[p1], $next[p2], $next[p3], 12);?>       
+</section>        
