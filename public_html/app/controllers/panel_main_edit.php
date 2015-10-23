b816281c5f2d6dd853278d5739b57c6a980cfaf3
HRUSTZ <hawk_vladimir@inbox.ru>
2015-10-23 21:17:36 +0300

    main edit

--- public_html/app/controllers/panel_main_edit.php
+++ public_html/app/controllers/panel_main_edit.php
@@ -1,20 +1,51 @@
 <?
-$action = $qwery[3];
+$action = $query[3];
 
-if($action == text_edit && $_POST != ''){
+if($action == 'text_edit' && $_POST != ''){
   $newTitle = $_POST[title];
   $newAbout = $_POST[about];
-}
-
-if($action == products_edit && $_POST != ''){
   
+  if($newTitle != '' && $newAbout != ''){
+    $mysqli -> query("UPDATE Data SET title = '$newTitle', about = '$newAbout' WHERE id = '1'");
+    header('Location: /panel/main_edit');
+    exit();
+  } else{
+    echo('Незаполнены все поля');
+  }
 }
 
-$q = $mysqli -> query("SELECT title, maintext FROM Data WHERE id = '0'");
-$result[text] = $q -> fetch_assoc;
+if($action == 'products_edit' && $_POST != ''){
+  $control = 0;
+  
+  foreach($_POST as $value){
+    if($value == '1')$control++;
+    if($value == '2')$control++;
+    if($value == '3')$control++;
+  }
+  
+  if($control == 3){
+    foreach ($_POST as $key => $value){
+      $mysqli -> query("UPDATE Products SET main = '$value' WHERE id = '$key'");
+    }
+    header('Location: /panel/main_edit');
+    exit();
+  } else{
+    echo('Порядок распределен неправильно');
+  }
+}
+
+$q = $mysqli -> query("SELECT title, about FROM Data");
+$result[text] = $q -> fetch_assoc();
 
 $q = $mysqli -> query("SELECT id, name, main FROM Products");
 $result[products] = $q -> fetch_all(MYSQLI_ASSOC);
 
 $title = "Редактирование главной страницы";
 $content = tpl("panel_main_edit", $result);
+
+    $id = $v;
+    $href = $_POST[href][$v];
+    $active = ($_POST[active][$v]=='on')?'1':'0';
+    $imgType = $_FILES[image][type][$v];
+    $urlFile = "/assets/banners/";
+    $imgQuery = '';
--- public_html/app/templates/panel_main_edit.php
+++ public_html/app/templates/panel_main_edit.php
@@ -1,14 +1,22 @@
-<form action="/profile_edit/panel/main_edit/text_edit" method="POST">
+<form action="/panel/main_edit/text_edit" method="POST">
   <input type="text" name="title" placeholder="Введитеp заголовок сайта:" value="<?=$cont[text][title]?>"/>
-  <textarea  name="mainText"><?=$cont[text][about]?></textarea>
+  <textarea name="about"><?=$cont[text][about]?></textarea>
   <input type="submit" value="Подтведить">
 </form>
 
-
-<form action="/profile_edit/panel/main_edit/products_edit" method="POST">
-  <? foreach($cont[products] as $value): ?> 
-  <?= $value[name]?>
-  <input type="text" name="nom1" placeholder="Введите номер вывода на странице:" value="<?=$value[main]?>"/>
-  <? endforeach; ?>
+<form action="/panel/main_edit/products_edit" method="POST">
+  <table>
+    <tr>
+      <th>Товар</th>
+      <th>Номер на главной</th>
+      <th>ID</th>
+    </tr>
+    <? foreach($cont[products] as $value): ?> 
+      <tr>
+        <td><?=$value[name]?></td>
+        <td><input type="text" name="<?=$value[id]?>" placeholder="Введите номер вывода на странице:" value="<?=$value[main]?>"/></td>
+      </tr>
+    <? endforeach; ?>
+  </table>
   <input type="submit" value="Подтведить">
 </form>
