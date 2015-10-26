fd944f42126970daf2c6430fde63db973f94d106
German Gorshkov <23lopushok@gmail.com>
2015-10-26 22:14:55 +0300

    fix

--- public_html/app/templates/article.php
+++ public_html/app/templates/article.php
@@ -1,6 +1,6 @@
 <article class="article-page wrap shadow">
-    <h1 class="article-title"><?= $cont[name]?></h1>
     <img class="article-img" src="<?= $cont[photo]?>" alt="<?= $cont[name]?>">
+    <h1 class="article-title"><?= $cont[name]?></h1>
     <p class="article-text"><?= $cont[text]?></p>
     <p class="article-date"><?echo rus_date("j F, Y", strtotime($cont[date]));?></p>
 </article>
