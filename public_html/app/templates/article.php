1044cfd1e0998e87fdf8a61efd6430f458e4076a
German Gorshkov <23lopushok@gmail.com>
2015-10-26 21:41:13 +0300

    article

--- public_html/app/templates/article.php
+++ public_html/app/templates/article.php
@@ -1,4 +1,15 @@
-<?= $cont[name]?>
-<img src="<?= $cont[photo]?>">
-<?= $cont[text]?>
-<?echo rus_date("j F, Y", strtotime($cont[date]));?>
+<article class="article-page wrap shadow">
+    <h1 class="article-title"><?= $cont[name]?></h1>
+    <img class="article-img" src="<?= $cont[photo]?>" alt="<?= $cont[name]?>">
+    <p class="article-text"><?= $cont[text]?></p>
+    <p class="article-date"><?echo rus_date("j F, Y", strtotime($cont[date]));?></p>
+</article>
+
+
+
+
+
+
+
+
+
