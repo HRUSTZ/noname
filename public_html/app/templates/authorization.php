9ff34a13362726395b4b832802048497475a6c10
HRUSTZ <hawk_vladimir@inbox.ru>
2015-10-31 17:46:36 +0300

    fix

--- public_html/app/templates/authorization.php
+++ public_html/app/templates/authorization.php
@@ -6,11 +6,11 @@
     <form action="/authorization" method="POST" class="sign-in-form form">
       <div>
         <label for="user-email">Email</label>
-        <input type="email" id="user-email" required>
+        <input type="email" id="user-email" name="login" required>
       </div>
       <div>
         <label for="password1">Пароль</label>
-        <input type="password" id="password1" required>
+        <input type="password" id="password1" name="password" required>
       </div>
       <button class="form-btn button button-anim" type="submit">Авторизация</button> 
       <?echo($cont[cont]);?>
--- public_html/app/templates/registration.php
+++ public_html/app/templates/registration.php
@@ -5,20 +5,20 @@
     </div>
     <form class="form sign-up-form" action="/registration" method="POST">
       <div>
-        <label for="name">Фамилия и имя</label>
-        <input type="text" id="name" required>
-      </div>
-      <div>
         <label for="user-email">Email</label>
-        <input type="email" id="user-email" required>
+        <input type="email" id="user-email" name="login" required>
       </div>
       <div>
         <label for="password1">Пароль</label>
-        <input type="password" id="password1" required>
+        <input type="password" id="password1" name="password1" required>
       </div>
       <div>
         <label for="password2">Повторите пароль</label>
-        <input type="password" id="password2" required>
+        <input type="password" id="password2" name="password2" required>
+      </div>
+      <div>
+        <label for="name">Ф.И.О.</label>
+        <input type="text" id="name" name="name" required>
       </div>
       <button class="form-btn button button-anim" type="submit">Регистрация</button> 
     </form>
