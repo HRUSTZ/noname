47007d929598ef5b5548519a5c5bc40d923b5605
German Gorshkov <23lopushok@gmail.com>
2015-11-01 13:43:38 +0300

    catalog

--- public_html/app/templates/products.php
+++ public_html/app/templates/products.php
@@ -1,27 +1,33 @@
 <section class="products">
   <div class="products-content">
     <section class="products-filter">
-      <h1>Категории</h1>
-      <a href="/products">Все товары</a>
+      <div class="page-title">
+        <h1><i class="fa fa-filter"></i>Категории</h1>
+      </div>
+      <p><a href="/products"><i class="fa fa-chevron-right"></i>Все товары</a></p>
       <? foreach($cont[cat] as $value): ?>
-        <a href="/products/<?= $value[name]?>"><?= $value[translation]?></a>
+      <p><a href="/products/<?= $value[name]?>"><i class="fa fa-chevron-right"></i><?= $value[translation]?></a></p>
       <? endforeach; ?>
     </section>
     <section class="products-catalog">
-      <h1><?= $cont[title]?></h1>
+      <div class="page-title">
+          <h1><i class="fa fa-shopping-cart"></i><?= $cont[title]?></h1>
+        </div>
       <section class="products-catalog-content">
         <? foreach($cont[cont] as $value): ?>         
         <article class="product">
           <a href="/products/number/<?= $value[id]?>"><img class="product-img" src="<?= $value[photo]?>" alt="<?= $value[name]?>"></a>
-          <h2 class="product-name"><a href="/products/number/<?= $value[id]?>"><?= $value[name]?></a></h2>
-          <p class="product-description"><?= $value[description]?></p>
+          <div class="product-nd">
+            <p class="product-name"><a href="/products/number/<?= $value[id]?>"><?= $value[name]?></a></p>
+            <p class="product-description"><?= $value[description]?></p>
+          </div>
           <p class="product-price"><?= $value[price]?><i class="fa fa-rub"></i></p>
         </article>
         <? endforeach; ?>
-        <?= $cont[clean]?>
+        <p class="products-clean"><?= $cont[clean]?></p>
       </section>
+    <?page($cont[page][0], $cont[page][1], $cont[page][2], $cont[page][3], $cont[page][4])?>
     </section>
   </div>        
-  <?page($cont[page][0], $cont[page][1], $cont[page][2], $cont[page][3], $cont[page][4])?>
 </section>       
 
