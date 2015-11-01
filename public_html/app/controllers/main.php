fcde21134d6cea97d2e41c4304bab3b6e31c4b52
HRUSTZ <hawk_vladimir@inbox.ru>
2015-11-01 14:11:45 +0300

    fix

--- public_html/app/controllers/main.php
+++ public_html/app/controllers/main.php
@@ -9,7 +9,7 @@ $result[servicesText] = array($txt[0], $txt[1], $txt[2]);
 $q = $mysqli -> query("SELECT * FROM News ORDER BY id DESC LIMIT 0, 3");
 $result[news] = $q -> fetch_all(MYSQLI_ASSOC);
 
-$q = $mysqli -> query("SELECT id, name, photo, price, main  FROM Products WHERE main = '1' || main = '2' || main = '3' ORDER BY main");
+$q = $mysqli -> query("SELECT id, name, description, photo, price, main  FROM Products WHERE main = '1' || main = '2' || main = '3' ORDER BY main");
 $result[products] = $q -> fetch_all(MYSQLI_ASSOC);
 
 $title = "Главная страница";
