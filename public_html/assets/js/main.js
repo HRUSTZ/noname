236cd570a4c4d4f015486e87cb35b1e401f93e65
German Gorshkov <23lopushok@gmail.com>
2015-11-05 19:58:28 +0300

    fix

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -24,39 +24,3 @@ function notice(text){
   document.body.removeChild(mask);
   } , 5000)
 }
-
-
-
-/*(function( window ){
- 
-  'use strict';
-
-  var body = document.body,
-      mask = document.createElement("div"),
-      showBlockLogin = document.querySelector( ".login" ),
-      activeNav;
-
-  mask.className = "mask";
-
-  showBlockLogin.addEventListener( "click", function(){
-      classie.add( body, "show-login" );
-      document.body.appendChild(mask);
-      activeNav = "show-login";
-  } );
-
-  mask.addEventListener( "click", function(){
-      classie.remove( body, activeNav );
-      activeNav = "";
-      document.body.removeChild(mask);
-  } );
- 
-
-  [].slice.call(document.querySelectorAll(".close-menu")).forEach(function(el,i){
-      el.addEventListener( "click", function(){
-          classie.remove( body, activeNav );
-          activeNav = "";
-          document.body.removeChild(mask);
-      } );
-  });
- 
-})( window );*/
