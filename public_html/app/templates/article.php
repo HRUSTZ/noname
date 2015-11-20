499a73d61d381c46ceef01e9e298e6c8013547ac
HRUSTZ <hawk_vladimir@inbox.ru>
2015-11-20 23:15:33 +0300

    fix

--- public_html/app/templates/article.php
+++ public_html/app/templates/article.php
@@ -5,15 +5,17 @@
     <div class="separator"></div>
     <p class="article-page-description"><?= $cont[text]?></p>
   </section>
-  <section class="article-page-sidebar shadow">
-    <h1>Последние акции</h1>
-    <? foreach($cont[more] as $value): ?>
-    <p class="article-sidebar-date"><?echo rus_date("j F Y", strtotime($cont[date]));?></p>
-    <p class="article-sidebar-name"><a href="/news/article/<?= $value[id]?>"><?= $value[name]?></a></p>
-    <? endforeach; ?>
-    <div class="separator"></div>
-    <p class="article-sidebar-link"><a href="/news">Все акции</a></p>
-  </section>
+  <? if($cont[more]):?>
+    <section class="article-page-sidebar shadow">
+      <h1>Последние акции</h1>
+      <? foreach($cont[more] as $value): ?>
+      <p class="article-sidebar-date"><?echo rus_date("j F Y", strtotime($cont[date]));?></p>
+      <p class="article-sidebar-name"><a href="/news/article/<?= $value[id]?>"><?= $value[name]?></a></p>
+      <? endforeach; ?>
+      <div class="separator"></div>
+      <p class="article-sidebar-link"><a href="/news">Все акции</a></p>
+    </section>
+  <?endif;?>
 </section>
 
 
